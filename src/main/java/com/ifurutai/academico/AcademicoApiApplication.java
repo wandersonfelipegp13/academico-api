fe689166619fc0b2fa5b7e07bb4e860db9aefdfb
Wanderson <wandersonfelipeplus13@gmail.com>
2021-07-20 23:03:20 -0300

    Commit inicial.

---
+++ src/main/java/com/ifurutai/academico/AcademicoApiApplication.java
@@ -0,0 +1,13 @@
+package com.ifurutai.academico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademicoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademicoApiApplication.class, args);
+	}
+
+}
