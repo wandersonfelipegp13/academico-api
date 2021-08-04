258e3394743b4b12888532ccee9f082cec42c310
Wanderson <wandersonfelipeplus13@gmail.com>
2021-08-03 23:42:40 -0300

    Criado método buscar no ProfessorController.

--- src/main/java/com/ifurutai/academico/api/controller/ProfessorController.java
+++ src/main/java/com/ifurutai/academico/api/controller/ProfessorController.java
@@ -1,9 +1,11 @@
 package com.ifurutai.academico.api.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import com.ifurutai.academico.domain.model.Professor;
 import com.ifurutai.academico.domain.repository.ProfessorRepository;
@@ -17,6 +19,14 @@ public class ProfessorController {
 	@GetMapping("/professores")
 	public List<Professor> listar() {
 		return profRepository.findAll();
+	}
+	
+	// Singleton resources
+	@GetMapping("/professores/{profId}") // path variable
+	public Professor buscar(@PathVariable Long profId) {
+		Optional<Professor> prof = profRepository.findById(profId);
+		// devolve um Optional do tipo Professor: pode ser vazio ou não
+		return prof.orElse(null);
 	}
 	
 }
