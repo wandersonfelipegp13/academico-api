9667f9d9becc6ff912a583a51559b8c4b246b456
Wanderson <wandersonfelipeplus13@gmail.com>
2021-08-03 23:26:01 -0300

    Controle de Professor ajustado com repositório.

--- src/main/java/com/ifurutai/academico/api/controller/ProfessorController.java
+++ src/main/java/com/ifurutai/academico/api/controller/ProfessorController.java
@@ -2,22 +2,21 @@ package com.ifurutai.academico.api.controller;
 
 import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.ifurutai.academico.domain.model.Professor;
+import com.ifurutai.academico.domain.repository.ProfessorRepository;
 
 @RestController
 public class ProfessorController {
 	
-	@PersistenceContext
-	private EntityManager manager;
+	@Autowired
+	private ProfessorRepository profRepository;
 	
 	@GetMapping("/professores")
 	public List<Professor> listar() {
-		return manager.createQuery("from Professor", Professor.class).getResultList();
+		return profRepository.findAll();
 	}
 	
 }
