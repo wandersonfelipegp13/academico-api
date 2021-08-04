dd57307640eb8ed5150802e7f90ce7ff8322ae97
Wanderson <wandersonfelipeplus13@gmail.com>
2021-08-03 23:33:06 -0300

    Criados métodos: busca por nome e email, no repositório de Professor.

--- src/main/java/com/ifurutai/academico/domain/repository/ProfessorRepository.java
+++ src/main/java/com/ifurutai/academico/domain/repository/ProfessorRepository.java
@@ -1,5 +1,7 @@
 package com.ifurutai.academico.domain.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,12 @@ import com.ifurutai.academico.domain.model.Professor;
 @Repository
 public interface ProfessorRepository extends JpaRepository<Professor, Long> {
 	
+	// Busca por nome que contém uma parte
+	List<Professor> findByNomeContaining(String nome);
+	
+	// Busca por nome
+	List<Professor> findByNome(String nome);
+	
+	// Busca um cliente por email
+	Professor findByEmail(String email);
 }
