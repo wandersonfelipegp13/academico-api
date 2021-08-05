11f7f3b88c0abae0890b5844c2a956bbda7e7403
Wanderson <wandersonfelipeplus13@gmail.com>
2021-08-05 03:11:10 -0300

    Aviso sobre campos inválidos: campos em branco.

--- src/main/java/com/ifurutai/academico/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/ifurutai/academico/api/exceptionhandler/ApiExceptionHandler.java
@@ -1,10 +1,13 @@
 package com.ifurutai.academico.api.exceptionhandler;
 
 import java.time.OffsetDateTime;
+import java.util.ArrayList;
 
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
@@ -16,10 +19,18 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 	@Override
 	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
 			HttpHeaders headers, HttpStatus status, WebRequest request) {
+		var campos = new ArrayList<Problema.Campo>();
+		for (ObjectError error : ex.getBindingResult().getAllErrors()) {
+			String nome = ((FieldError) error).getField();
+			String msg = error.getDefaultMessage();
+			campos.add(new Problema.Campo(nome, msg));
+		}
+		
 		var problema = new Problema();
 		problema.setStatus(status.value());
 		problema.setTitulo("Um ou mais campos estão inválidos. Faça o preenchimento correto e tente novamente!");
 		problema.setDataHora(OffsetDateTime.now());
+		problema.setCampos(campos);
 		return super.handleExceptionInternal(ex, problema, headers, status, request);
 	}
 	
