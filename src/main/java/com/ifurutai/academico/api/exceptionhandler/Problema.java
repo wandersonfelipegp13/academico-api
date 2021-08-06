f04a0a515635f221947673432397e1c7e056a9a3
Wanderson <wandersonfelipeplus13@gmail.com>
2021-08-06 04:40:25 -0300

    Problema sem retorno null de campos.

--- src/main/java/com/ifurutai/academico/api/exceptionhandler/Problema.java
+++ src/main/java/com/ifurutai/academico/api/exceptionhandler/Problema.java
@@ -3,6 +3,10 @@ package com.ifurutai.academico.api.exceptionhandler;
 import java.time.OffsetDateTime;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+@JsonInclude(Include.NON_NULL)
 public class Problema {
 	
 	private Integer status;
