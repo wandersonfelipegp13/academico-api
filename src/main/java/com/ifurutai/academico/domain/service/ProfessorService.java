7f411924c81db38cb004a8f8a694990a76b5cfff
Wanderson <wandersonfelipeplus13@gmail.com>
2021-08-06 02:52:08 -0300

    Iterface de serviço de Professor.

---
+++ src/main/java/com/ifurutai/academico/domain/service/ProfessorService.java
@@ -0,0 +1,13 @@
+package com.ifurutai.academico.domain.service; 
+import java.util.List;
+import com.ifurutai.academico.domain.model.Professor;
+
+public interface ProfessorService {
+	public abstract Professor inserirProfessor(Professor prof);
+	public abstract Professor atualizarProfessor(Long profId, Professor prof);
+	public abstract Professor excluirProfessor(Long profId);
+	public abstract List<Professor> buscarProfessores();
+	public abstract Professor buscarProfessorPorId(Long profId);
+	public abstract Professor buscarProfessorPorEmail(String email);
+	public abstract Boolean existeProfessorPorId(Long profId);
+}
