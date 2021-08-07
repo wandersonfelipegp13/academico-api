edcf4589624daedf1d4e5d32410a2ccd099004b3
Wanderson <wandersonfelipeplus13@gmail.com>
2021-08-06 23:59:40 -0300

    Service de Curso.

---
+++ src/main/java/com/ifurutai/academico/domain/service/CursoService.java
@@ -0,0 +1,16 @@
+package com.ifurutai.academico.domain.service;
+
+import java.util.List;
+
+import com.ifurutai.academico.domain.model.Curso;
+
+public interface CursoService {
+	
+	public abstract Curso inserirCurso();
+	public abstract Curso atualizarCurso(Long cursoId, Curso curso);
+	public abstract Curso excluirCurso(Long cursoId);
+	public abstract List<Curso> buscarCurso();
+	public abstract Curso buscarCursoPorId(Long cursoId);
+	public abstract Boolean existeCursoPorId(Long cursoId);
+	
+}
