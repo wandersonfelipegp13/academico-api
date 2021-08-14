18a51b56d545ee07d00627afaa06efdf3f5184e1
Wanderson <wandersonfelipeplus13@gmail.com>
2021-08-13 23:21:05 -0300

    ProfessorResumoModel

--- src/main/java/com/ifurutai/academico/domain/model/ProfessorResumoModel.java
+++ src/main/java/com/ifurutai/academico/domain/model/ProfessorResumoModel.java
@@ -3,5 +3,42 @@ package com.ifurutai.academico.domain.model;
 public class ProfessorResumoModel {
 
 	private Long id;
+	private String nome;
+	private String titulacao;
+
+	public ProfessorResumoModel() {
+		super();
+	}
 	
+	public ProfessorResumoModel(Long id, String nome, String titulacao) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.titulacao = titulacao;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getTitulacao() {
+		return titulacao;
+	}
+
+	public void setTitulacao(String titulacao) {
+		this.titulacao = titulacao;
+	}
+
 }
