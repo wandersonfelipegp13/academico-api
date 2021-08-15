c6fa05f10ac0f7e2917395148793428ba3bc61ac
Wanderson <wandersonfelipeplus13@gmail.com>
2021-08-15 19:20:29 -0300

    Classe AlunoRepository criada.

---
+++ src/main/java/com/ifurutai/academico/domain/repository/AlunoRepository.java
@@ -0,0 +1,17 @@
+package com.ifurutai.academico.domain.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ifurutai.academico.domain.model.Aluno;
+
+@Repository
+public interface AlunoRepository extends JpaRepository<Aluno, Long> {
+	
+	List<Aluno> findByNomeContaining(String nome);
+	List<Aluno> findByNome(String nome);
+	Aluno findByEmail(String email);
+
+}
